112eb050054f650c94fa48bb0a0cc72a1214a3cf
Jose Mauro <50158799+VoidEyes@users.noreply.github.com>
2019-05-01 16:15:47 -0300

    StopWatch v1
    Primeira implementacao do StopWatch

---
+++ app/src/main/java/com/dfz/stopwatch/StopwatchActivity.java
@@ -0,0 +1,56 @@
+package com.dfz.stopwatch;
+//
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class StopwatchActivity extends AppCompatActivity {
+    //Segundos no cronometro
+    private int seconds=0;
+    //Verifica o estado do cronometro
+    private boolean running;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stopwatch);
+        runTimer();
+    }
+
+    //Inicia o cronometro
+    public void Start(View view){
+        running = true;
+    }
+
+    //Para o cronometro
+    public void Stop(View view){
+        running = false;
+    }
+
+    //Reinicia o cronometro
+    public void Reset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    private void runTimer(){
+        final TextView timeView = (TextView)findViewById(R.id.time_view);
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {//Handler permite que o aplicativo rode em segundo plano.
+            @Override
+            public void run() {
+                int hours = seconds/3600;
+                int minutes = (seconds%3600)/60;
+                int secs = seconds%60;
+                String time = String.format("%d:%02d:%02d", hours, minutes, secs);
+                timeView.setText(time);
+                if (running){
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000); //chama o handler novamente apos 1000 milesegundos
+            }
+        });
+    }
+}
